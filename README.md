70675401ec463dd9b76e35c9eaa3026ffb6145b3
Sezgin Erpaytoncu <sezginerpaytoncu@gmail.com>
2025-03-27 16:13:48 +0300

    initial commit for project setup

---
+++ README.md
@@ -0,0 +1,40 @@
+# Movie App
+
+A React application that allows users to search and view movie details using the OMDb API.
+
+## Features
+
+- Search movies by title
+- Filter by year and type (Movie, TV Series, Episode)
+- View detailed movie information
+- Responsive design
+- Pagination support
+
+## Getting Started
+
+1. Clone the repository
+2. Install dependencies:
+   ```bash
+   npm install
+   ```
+3. Create a `.env` file in the root directory and add your OMDb API key:
+   ```
+   REACT_APP_OMDB_API_KEY=your_api_key_here
+   ```
+4. Start the development server:
+   ```bash
+   npm start
+   ```
+
+## API Reference
+
+This project uses the [OMDb API](http://www.omdbapi.com/). Get your API key at [http://www.omdbapi.com/apikey.aspx](http://www.omdbapi.com/apikey.aspx).
+
+## Tech Stack
+
+- React
+- TypeScript
+- Redux Toolkit
+- React Router
+- SCSS
+- Bootstrap
---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './store/store';
+import MovieList from './components/MovieList';
+import MovieDetails from './components/MovieDetails';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.scss';
+
+const App: React.FC = () => {
+  return (
+    <Provider store={store}>
+      <Router>
+        <div className="app">
+          <header className="app-header">
+            <h1>Movie App</h1>
+          </header>
+          <main className="app-main">
+            <Routes>
+              <Route path="/" element={<MovieList />} />
+              <Route path="/movie/:imdbID" element={<MovieDetails />} />
+            </Routes>
+          </main>
+        </div>
+      </Router>
+    </Provider>
+  );
+};
+
+export default App; 
